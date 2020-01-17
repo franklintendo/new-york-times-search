73576d375b25507428983feacc16da493c914278
Satinder Singh <spalsingh91@gmail.com>
2020-01-17 12:06:21 -0500

    commiting before pulling master

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
     // 1. The user interfaces with the above layout and enters in the appropriate search terms
         // user enters a string, chooses a drop down option and then enters 2 year inputs
 
-var userString = "   test string";
-// once userstring is passed through a helper function it should look like "test+string"
 
-function cleanString (userString) {
-    var cleanUserString = "";
-    cleanUserString = userString.trim();
-    cleanUserString = cleanUserString.replace(" ","+");
-    
-    return cleanUserString;
-};
-    
-console.log(cleanString(userString));
+// Search term
+    var userString = "   test string";
+    // once userstring is passed through a helper function it should look like "test+string"
 
+    function cleanString (userString) {
+        var cleanUserString = "";
+        cleanUserString = userString.trim();
+        cleanUserString = cleanUserString.replace(" ","+");
+        
+        return cleanUserString;
+    };
+        
+    console.log(cleanString(userString));
+
+// Number of searches
+    var numberOfSearches = "";
+
+// Start and End Year
+    var startYear = "1999";
+    
