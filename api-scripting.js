e13e22c69647e3d8f3be5d84e92ea9a57a0480c3
Frank Moran <info@francisfrankmoran.com>
2020-01-17 11:30:44 -0500

    api key and search reference

---
+++ api-scripting.js
@@ -0,0 +1,17 @@
+var APIkey = "0n6rRadT3Dw9bHSjuJMAcBwlNglDHZgK";
+
+// PARAMETERS
+// _____________________________
+// Search term (what user enters)
+// Number of records the user wants (within a certain number)
+// Start year *OPTIONAL*
+// End Year *OPTIONAL*
+
+// Example of a call
+// _____________________________
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+//
+// "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTermVariable + "&api-key=0n6rRadT3Dw9bHSjuJMAcBwlNglDHZgK"
+// 
+
+
